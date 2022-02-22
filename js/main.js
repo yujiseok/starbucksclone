ebf0d06d76a0efdd63ed507bf5ee2fd00112a9f3
yujiseok <koesijuy@gmail.com>
2022-02-23 00:26:12 +0900

    22.02.23 commit

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ searchEl.addEventListener('click', function() {
   searchInputEl.focus();
 });
 
-
 searchInputEl.addEventListener('focus', function() {
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색')
@@ -14,4 +13,33 @@ searchInputEl.addEventListener('focus', function() {
 searchInputEl.addEventListener('blur', function() {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '')
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function() {  // _.throttle(함수, 시간) -- > 함수 실행 횟수 제한
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+    //gsap.to(요소, 시간, 옵션)
+    // 배지 숨기기
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+    // 배지 보이기 
+  }
+}, 300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7, 1.4, 2.1, 2.7 순서로 나타남
+    opacity: 1
+  });
 });
